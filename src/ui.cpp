513f80f6a4fcbf1aca083f39078d1c504ebf8d9a
Dominik Rzecki <dominik.rzecki@gmail.com>
2020-04-24 19:25:07 +0200

    updated ui class

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-#include <gtkmm-3.0/gtkmm.h>
+
 #include "./ui.hpp"
 
 Ui::Ui (int argc, char* argv[]){
-  app = Gtk::Application::create(argc, argv);
-  builder = Gtk::Builder::create();
-  try{
-      builder->add_from_file("./src/glade/logix.glade");
-  }catch(const Glib::FileError& ex){
+	app = Gtk::Application::create(argc, argv);
+	builder = Gtk::Builder::create();
+	try{
+		builder->add_from_file("./src/res/glade/logix.glade");
+	}catch(const Glib::FileError& ex){
       std::cerr << "FileError: " << ex.what() << std::endl;
       Gtk::Main::quit();
-  }catch(const Glib::MarkupError& ex){
+	}catch(const Glib::MarkupError& ex){
       std::cerr << "MarkupError: " << ex.what() << std::endl;
       Gtk::Main::quit();
-  }catch(const Gtk::BuilderError& ex){
+	}catch(const Gtk::BuilderError& ex){
       std::cerr << "BuilderError: " << ex.what() << std::endl;
       Gtk::Main::quit();
-  }
-  builder->get_widget("mainWindow", window);
-  builder->get_widget("glArea", glArea);
+	}
+	builder->get_widget("mainWindow", window);
+	builder->get_widget("glArea", glArea);
+	viewport = new Renderer(glArea);
+}
+
+Ui::~Ui(){
+	delete viewport;
 }
 
 Gtk::Window* window = nullptr;
 Gtk::GLArea* glArea = nullptr;
-Glib::RefPtr<Gtk::Builder> builder;
-Glib::RefPtr<Gtk::Application> app;
+Renderer* viewport = nullptr;
--- src/ui.hpp
+++ src/ui.hpp
@@ -1,13 +1,17 @@
 #ifndef UI_H
 #define UI_H
 
+#include "./renderer.hpp"
 #include <gtkmm-3.0/gtkmm.h>
 
 class Ui{
 	public:
 		Ui(int argc, char* argv[]);
+		~Ui();
 		Gtk::Window* window;
-    	Gtk::GLArea* glArea;
+		Gtk::Paned* paned;
+		Renderer* viewport;
+		Gtk::GLArea* glArea;
     	Glib::RefPtr<Gtk::Application> app;
 	private:
 		Glib::RefPtr<Gtk::Builder> builder;
