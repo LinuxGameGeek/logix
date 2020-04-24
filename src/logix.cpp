7a1e55c92c6607210e5bf10104ddbe4e413551f7
Dominik Rzecki <dominik.rzecki@gmail.com>
2020-04-24 19:25:26 +0200

    updated main

--- src/logix.cpp
+++ src/logix.cpp
@@ -1,7 +1,8 @@
-#include "ui.hpp"
+#include "./ui.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[]){
+	std::cout<<".\n";
 	Ui window(argc, argv);
 	return window.app->run(*window.window);
 }
