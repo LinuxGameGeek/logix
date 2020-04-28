c478a85abec929c7aa3da234d294ebcec392dfe8
Dominik Rzecki <dominik.rzecki@gmail.com>
2020-04-28 23:21:26 +0200

    the renderer now uses vertexbufferlayout and vertexarray objects

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,9 +1,6 @@
 //#include <ui.hpp>
 #include "./renderer.hpp"
 
-#include <iostream>
-#include <epoxy/gl.h>
-
 Renderer::Renderer(Gtk::GLArea* glarea)
 :glArea(glarea){
  	glArea->signal_realize().connect(sigc::mem_fun(*this, &Renderer::realize));
@@ -13,11 +10,51 @@ Renderer::Renderer(Gtk::GLArea* glarea)
 }
 
 void Renderer::realize(){
-	std::clog<<glGetString(GL_VERSION)<<"\n";
+	std::cout<<glGetString(GL_VERSION)<<std::endl;
 	glArea->make_current();
 	std::cout<<"realize\n";
 	try{
 		glArea->throw_if_error();
+
+		shader_program = Shader::create_shader_program(nullptr);
+		glUseProgram(shader_program);
+
+		GLfloat pos[] = {
+			-0.5f, -0.5f, -1,
+			0.5f, -0.5f, -1,
+			0.5f, 0.5f, -1,
+			-0.5f, 0.5f, -1
+		};
+		GLuint ind[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+
+		vaptr = new VertexArray();
+
+		//glGenVertexArrays(1, &vao);
+		//glBindVertexArray(vao);
+
+		vbptr = new VertexBuffer(pos, 4 * 3 * sizeof(GLfloat));
+
+		vblptr = new VertexBufferLayout;
+		vblptr->add(3, GL_FLOAT);
+		//vblptr->add(3, GL_FLOAT);
+		//vblptr->add(2, GL_FLOAT);
+		vaptr->addBuffer(*vbptr, *vblptr);
+		//glEnableVertexAttribArray(0);
+		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, 0);
+
+		ibptr = new IndexBuffer(ind, 6);
+
+		int location = epoxy_glGetUniformLocation(shader_program, "u_Color");
+		glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
+
+
+	 	glBindVertexArray(0);
+ 		glUseProgram(0);
+ 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	}catch(const Gdk::GLError& gle){
 		std::cerr << "An error occured making the context current during realize:" << std::endl;
 		std::cerr << gle.domain() << "-" << gle.code() << "-" << gle.what() << std::endl;
@@ -25,11 +62,12 @@ void Renderer::realize(){
 }
 
 void Renderer::unrealize(){
+	delete vbptr;
+	delete ibptr;
 	glArea->make_current();
 	try
 	{
-		windowPtr->glArea->throw_if_error();
-		delete rect;
+		glArea->throw_if_error();
 	}
 	catch(const Gdk::GLError& gle)
 	{
@@ -43,6 +81,15 @@ void Renderer::resize(int width, int height){
 }
 
 bool Renderer::render(const Glib::RefPtr<Gdk::GLContext>&  context ){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(shader_program);
 
+	//glBindVertexArray(vao);
+	vaptr->bind();
+	ibptr->bind();
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+	glBindVertexArray(0);
+	glUseProgram(0);
 	return true;
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -1,6 +1,13 @@
 #ifndef RENDERER_H
 #define RENDERER_H
-
+/*
+ This is the renderer of the viewport
+*/
+#include "viewport/shader.hpp"
+#include "viewport/vertexbuffer.hpp"
+#include "viewport/indexbuffer.hpp"
+#include "viewport/vertexarray.hpp"
+#include <iostream>
 #include <gtkmm-3.0/gtkmm/glarea.h>
 #include <epoxy/gl.h>
 
@@ -13,6 +20,12 @@ class Renderer{
     	bool render(const Glib::RefPtr<Gdk::GLContext>&);
 	private:
 		Gtk::GLArea* glArea;
+		GLuint shader_program;
+		GLuint vao;
+		IndexBuffer* ibptr;
+		VertexBuffer* vbptr;
+		VertexArray* vaptr;
+		VertexBufferLayout* vblptr;
 };
 
 #endif //
